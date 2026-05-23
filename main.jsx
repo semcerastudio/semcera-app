92e2b21f660f572d9fd000295d9287ea00b0637f
semcerastudio <semceraestudio@gmail.com>
2026-05-23 14:05:49 -0300

    Add files via upload

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from '../semcera-app.jsx'
+import App from './semcera-app.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
